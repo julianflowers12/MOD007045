71c5c9fe96e34b5f8cdcbc28ea250631da31ef31
julianflowers12 <julian.flowers13@gmail.com>
2022-01-31 12:26:49 +0000

    first commit

---
+++ MOD007045/README.md
@@ -0,0 +1,5 @@
+# MOD007045
+
+A [workflowr][] project.
+
+[workflowr]: https://github.com/workflowr/workflowr
---
+++ MOD007045/code/ssag.R
@@ -0,0 +1,13 @@
+## using semantic scholar academic graph API
+
+get_ss_papers <- function(search = "covid vaccination", offset = 0, `next` = 100){
+
+  require(jsonlite)
+  require(stringr)
+  search <- str_replace(search, "\\s", "+")
+  url <- paste0("https://api.semanticscholar.org/graph/v1/paper/search?query=", search, "&", offset, "&", `next` )
+  df <- fromJSON(url, simplifyDataFrame = TRUE)
+  df
+}
+
+get_ss_papers("microplastics health")
