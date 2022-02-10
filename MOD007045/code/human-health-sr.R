4b48da243b40fcd751d5ea5c8e7f48655e699cd7
julianflowers12 <julian.flowers13@gmail.com>
2022-02-10 12:22:31 +0000

    updates

--- MOD007045/code/human-health-sr.R
+++ MOD007045/code/human-health-sr.R
@@ -1,10 +1,10 @@
 ## microplastics and human health
 
-library(myScrapers)
+library(myScrapers); library(europepmc); library(tidypmc)
 
-search <- "microplastics AND (health) human[mh] systematic[sb]"
+search <- "plasticenta"
 
-n <- 3
+n <- 13
 start <- 2000
 end <- 2022
 
@@ -12,4 +12,61 @@ key <- Sys.getenv("ncbi_key")
 
 res <- pubmedAbstractR(search = search, n = n, start = start, end = end, ncbi_key = key)
 
-res$abstracts$title
+res$abstracts$abstract
+
+pmc <- pluck(res$abstracts, "pmid")
+pmc <- map(pmc, epmc_details) %>%
+  map(., "basic")
+
+glimpse(pmc)
+
+
+search1 <- "plastic pollution health human[mh] review[pt ]"
+
+n <- 146
+start <- 2000
+end <- 2022
+
+key <- Sys.getenv("ncbi_key")
+
+res1 <- pubmedAbstractR(search = search1, n = n, start = start, end = end, ncbi_key = key)
+
+abs <- res1$abstracts
+
+anno <- myScrapers::annotate_abstracts(abs$abstract, abs$pmid)
+np <- myScrapers::abstract_nounphrases(anno)
+
+glimpse(np)
+
+np %>%
+  drop_na(term) %>%
+  group_by(doc_id) %>%
+    summarise(term = paste(term, collapse = ", ")) %>%
+  left_join(abs, by = c("doc_id" = "pmid")) %>%
+  gt::gt()
+
+
+search2 <- "microplastic pollution public health human[mh] review[pt ]"
+
+n <- 146
+start <- 2000
+end <- 2022
+
+key <- Sys.getenv("ncbi_key")
+
+res2 <- pubmedAbstractR(search = search2, n = n, start = start, end = end, ncbi_key = key)
+
+abs1 <- res2$abstracts
+
+anno1 <- myScrapers::annotate_abstracts(abs1$abstract, abs1$pmid)
+np1 <- myScrapers::abstract_nounphrases(anno1)
+
+glimpse(np1)
+
+np1 %>%
+  drop_na(term) %>%
+  group_by(doc_id) %>%
+  summarise(term = paste(term, collapse = ", ")) %>%
+  left_join(abs1, by = c("doc_id" = "pmid")) %>%
+  gt::gt()
+
